cff8137c88c4ac996d0f098e20def7fb165945a7
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2025-03-16 08:04:50 +0700

    feat: add header dummy home screen

--- mobile/app/(root)/(tabs)/index.tsx
+++ mobile/app/(root)/(tabs)/index.tsx
@@ -1,9 +1,22 @@
-import { Text, View } from 'react-native';
+import { icons, images } from '@/constants';
+import { Image, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-3xl font-rubik text-secondary">Welcome to BlackBox Camp</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <View className="px-5">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row items-center">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-col items-start ml-2 justify-center">
+              <Text className="text-white text-xs font-rubik">Selamat Pagi</Text>
+              <Text className="text-white text-base font-rubik-medium">Rizkyrachman</Text>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-6" tintColor="white" />
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
